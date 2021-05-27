7fea582407bdd052ac17e3b8f3105f2272fd2e07
ehcyno1 <ehcyno1@gmail.com>
2021-05-27 13:33:32 +0900

    Delete currency.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,8 @@ import App from './App.vue'
 import router from './routes/index.js';
 import vuetify from './plugins/vuetify';
 import store from './store/index.js';
-import {currency} from './utils/currency';
 
 Vue.config.productionTip = false;
-Vue.filter('currency', currency);
-
 
 new Vue({
     router,
